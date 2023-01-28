e75d49ca536e4aa6cd0ef2ffa28bde11ec79e84f
Enzo Gabriel <egabrielrbm@gmail.com>
2023-01-27 23:15:24 -0300

    Versão Pre-Alpha (v0.1)

--- P2.py
+++ P2.py
@@ -11,6 +11,19 @@ for count in np.arange(0, 10, 0.1):
     n2 = (p3 + p4) / 2
     mf = (2 * n1 + 3 * n2) / 5
     if mf >= 6:
-        print(f"Você precisa de, no mínimo, {p3: .2f} na p3 e {p4: .2f} na p4 para passar")
+        print(f"Você precisa de, no mínimo, {p3: .2f} na p3 e {p4: .2f} na p4 para passar sem ir para a AF")
         break
-
+for count in np.arange(0, 10, 0.1):
+    p3 = count + 0.1
+    p4 = count + 0.1
+    n2 = (p3 + p4) / 2
+    mf = (2 * n1 + 3 * n2) / 5
+    if mf >= 3:
+        print(f"Você precisa de, no mínimo, {p3: .2f} na p3 e {p4: .2f} na p4 para ir para a AF")
+        break
+for count in np.arange(0,10,0.1):
+    AF = count + 0.1
+    maf = (mf + AF) / 2
+    if maf >= 5:
+        print(f"Se você tirar a nota mínima para ir para a AF precisará de, no mínimo, {AF: .2f} para passar")
+        break
